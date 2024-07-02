4817877ee5771b732eadaa3a608c01ed83c33bcc
HENG-Menghy <hengmenghy2002@gmail.com>
2024-07-02 22:55:11 +0700

    finished on doing respondsive home page project

--- responsive-home-page/tailwind.config.js
+++ responsive-home-page/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        'auto-fit-220': 'repeat(auto-fit, minmax(220px, 1fr))'
+      },
       fontFamily: {
         'montserrat': ['Montserrat'],
       },
